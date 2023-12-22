f5fc890ad636d1cf493d5a30266aea0b355657a3
AssertionBit <markushasorokin@yandex.com>
2023-12-22 20:45:29 +0300

    WiP(server): Subjects retrieval methods initiated

--- server/http/data-endpoints.go
+++ server/http/data-endpoints.go
@@ -19,6 +19,29 @@ type authResponseT struct {
   Type model.UserType `json:"type"`
 }
 
+type documentResponseT struct {
+  Id uint `json:"id"`
+  Name string `json:"name"`
+  Status model.DocumentT `json:"status"`
+  BacktrackID string `json:"backtrack-id"`
+}
+
+type subjectResponseT struct {
+  Name string `json:"name"`
+  DocumentID []uint `json:"documents-id"`
+}
+
+type subjectResponseFullT struct {
+  Name string `json:"name"`
+  Documents []documentResponseT `json:"documents"`
+}
+
+/// authPostEndpoint takes raw JSON type and prcesses it 
+/// for creation of user. If user with such credentials
+/// exists, then user will not be created.
+///
+/// Params:
+///    ctx - Context of request/response from fiber
 func authPostEndpoint(ctx *fiber.Ctx) error {
   var authRequest authRequestT
   if err := json.Unmarshal(ctx.Body(), &authRequest); err != nil {
@@ -57,6 +80,7 @@ func authPostEndpoint(ctx *fiber.Ctx) error {
     zap.Timep("When", &possibleUser.CreatedAt),
   )
 
+  // All is going okay
   ctx.SendStatus(200)
   if res, err := json.Marshal(&authResponseT{
     Name: possibleUser.Name,
@@ -83,15 +107,107 @@ func userGetEndpoint(ctx *fiber.Ctx) error {
   return ctx.SendStatus(200)
 }
 
+/// subjectGetEndpoint function retrieves information single
+/// subject and returns all information about his documents.
+/// 
+/// Param:
+///   id  - Id of subject
+///   ctx - request/response context from fiber
+///
+/// Return:
+///   JSON object when possible 
 func subjectGetEndpoint(ctx *fiber.Ctx) error {
-  return ctx.SendStatus(200)
+  var possibleSubject model.SubjectModel
+
+  id, err := ctx.ParamsInt("id", -1)
+  if err != nil {
+    logger.Warn(
+      "Malformed path param (possibly missing)",
+      zap.String("Request-type", "retrieve single subject"),
+    )
+  }
+
+  // Seaching for single subject, error means not exists
+  if err := db.Model(&model.SubjectModel{}).Where("id = ?", id).First(&possibleSubject).Error; err != nil {
+    logger.Info(
+      "User not found with such id", 
+      zap.Int("Subject-Id", id),
+      zap.String("Request-type", "retrieve single subject"),
+    )
+
+    return ctx.SendStatus(404)
+  }
+
+  // Processing for response
+  data := subjectResponseFullT{
+    Name: "",
+    Documents: []documentResponseT{},
+  }
+
+  for _, doc := range possibleSubject.DocumentModels {
+    data.Documents = append(data.Documents, documentResponseT{
+      Id: doc.ID,
+      BacktrackID: doc.BacktrackId,
+      Name: doc.Name,
+      Status: doc.Status,
+    })
+  }
+
+  if res, err := json.Marshal(&data); err != nil {
+    return ctx.SendStatus(500)
+  } else {
+    _ = ctx.SendStatus(200)
+    return ctx.Send(res)
+  }
+}
+
+/// subjectsAllGetEndpoint function retrieves all subjects
+/// and returns array of their files, and them selfs. Function serves
+/// as just mark that document will be processed. For general data upload,
+/// use `/api/v1/upload` endpoint or uploadDocumentEndpoint function
+///
+/// Params:
+///   ctx - Context for request/response from fiber.
+func subjectsAllGetEndpoint(ctx *fiber.Ctx) error {
+  var subjects []model.SubjectModel = make([]model.SubjectModel, 0)
+
+  if err := db.Model(&model.SubjectModel{}).Find(&subjects).Error; err != nil {
+    logger.Info("No users found, returning empty array")
+    if res, err := json.Marshal(&subjects); err != nil {
+      return ctx.SendStatus(500)
+    } else {
+      return ctx.Send(res)
+    }
+  }
+
+  var subjectRespo []subjectResponseT = make([]subjectResponseT, 0)
+
+  logger.Info("Processing all subjects", zap.Int("Length", len(subjects)))
+  for _, subj := range subjects {
+    docId := make([]uint, 0)
+    for _, doc := range subj.DocumentModels {
+      docId = append(docId, doc.ID)
+    }
+
+    subjectRespo = append(subjectRespo, subjectResponseT{
+      Name: "",
+      DocumentID: docId,
+    })
+  }
+
+  logger.Info(
+    "Processed data succesfully", 
+    zap.String("Request-type", "Retreiving all subjects"),
+  )
+  if res, err := json.Marshal(subjectRespo); err != nil {
+    return ctx.SendStatus(500)
+  } else {
+    _ = ctx.SendStatus(200)
+    return ctx.Send(res)
+  }
 }
 
 func subjectPostEndpoint(ctx *fiber.Ctx) error {
-  return ctx.SendStatus(200)
-}
-
-func subjectsAllGetEndpoint(ctx *fiber.Ctx) error {
   return ctx.SendStatus(200)
 }
 
