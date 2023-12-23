68739b63de93e8c2eb85623240edf933e100ac55
AssertionBit <markushasorokin@yandex.com>
2023-12-23 05:11:08 +0300

    WIP(sub-system): Base entity

--- pdf-sub-system/lebata/pdf/data.py
+++ pdf-sub-system/lebata/pdf/data.py
@@ -1,32 +1,39 @@
 __all__ = (
-    "PageInfo",
+    "PdfPage",
+    "PdfImage",
+    "PdfText",
+    "PdfDocument"
 )
 
 from dataclasses import dataclass as _structure
+from typing import List as _List
+from typing import Union as _Union
 
 from pypdf import PageObject as _PdfPageObject
 from pypdf.generic import TextStringObject as _TextObject
+from reportlab.lib.units import cm as _cm
+from reportlab.pdfgen.canvas import Canvas as _PdfCanvas
 
 
-@_structure
-class Metadata(object):
-    height: int
-    width:  int
+class PdfText(object):
+    ...
 
 
-class PageInfo(object):
+class PdfImage(object):
+    ...
+
+
+class PdfPage(object):
     """
     @brief Page information object
     @field __page_number__ Number of page
     @field __page_text__ Text content of page
     """
 
-    __page_metadata__: Metadata
-    __page_number__: int
-    __page_text__:   str
+    __page_number__: int = -1
+    __page_objects__: _List[_Union[PdfText, PdfImage]] = []
 
     def __init__(self, 
-                 meta: Metadata, 
                  number: int,
                  text: str) -> None:
         """
@@ -36,17 +43,47 @@ class PageInfo(object):
 
         self.__page_text__ = text
         self.__page_number__ = number
-        self.__page_metadata__ = meta
 
-    def to_pdf_page(self) -> _PdfPageObject:
+    def to_pdf_page(self, canvas: _PdfCanvas) -> None:
         """
-        @brief Convert this object to native PyPDF object
+        @brief Draw content of this file to pdf canvas
+        @param canvas New PDF document to work with
         @return Insertable and processable PDF page
         """
 
-        page = _PdfPageObject.create_blank_page(None, 
-                                                self.__page_metadata__.width, 
-                                                self.__page_metadata__.height)
+        ...
 
-        return page
+
+class PdfDocument(object):
+    """
+    @brief Representation of PDF document which could be readed
+        and converted to pdf and back
+    """
+
+    __doc_name__: str
+    __doc_path__: str
+    __doc_pages__: _List[PdfPage]
+
+    def __init__(self, path: str) -> None:
+        ...
+
+    def build(self, out: str) -> None:
+        """
+        @brief Compiles back object into PDF
+        """
+
+    def __add__(self, o: PdfPage) -> None:
+        ...
+
+    def __iter__(self) -> "PdfDocument":
+        self.__iterration_count__ = 0
+        return self
+
+    def __next__(self) -> PdfPage:
+        self.__iterration_count__ += 1
+
+        if len(self.__doc_pages__) == self.__iterration_count__:
+            raise StopIteration
+
+        return self.__doc_pages__[self.__iterration_count__]
 
